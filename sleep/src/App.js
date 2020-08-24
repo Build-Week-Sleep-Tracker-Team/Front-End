d0f2376a9096b899bc6bfc7d02fa77f538958425
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-08-24 14:43:41 -0400

    getting started.

---
+++ sleep/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import{Route, Link} from 'react-router-dom'
+import './App.css';
+import SleepApp from'./'
+
+const App = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <button><Link to="/">Home</Link></button>
+        <button><Link to="/pizza">Tracker</Link></button>
+      </nav>
+      <h1>Weekly Sleep Tracker</h1>
+      <img src={image} alt= "pizza"></img>
+      <Route path="/pizza" component={SleepApp}/>
+    </>
+  );
+};
+export default App;
